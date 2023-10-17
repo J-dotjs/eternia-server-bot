f464f04f1bb16155c157a692b008db444910a960
Jared <jrb32002@gmail.com>
2023-10-17 15:28:56 -0400

    add try catch

--- functions/serverQuery.js
+++ functions/serverQuery.js
@@ -3,63 +3,69 @@ const util = require('minecraft-server-util')
 
 async function execute(client) {
 
-    var statusMSG = await client.channels.cache.get("1120547962435678298").messages.fetch("1161490303384043601")
+    try {
 
-    var statusEmbed = new EmbedBuilder()
-        .setColor('#964B00')
-        .setAuthor({ name: `${client.user.username}`, iconURL: `${client.user.displayAvatarURL()}` })
+        var statusMSG = await client.channels.cache.get("1120547962435678298").messages.fetch("1161490303384043601")
 
-    statusMSG.edit({ embeds: [statusEmbed] })
+        var statusEmbed = new EmbedBuilder()
+            .setColor('#964B00')
+            .setAuthor({ name: `${client.user.username}`, iconURL: `${client.user.displayAvatarURL()}` })
 
-    setInterval(async () => {
-        util.status('eternia.serveminecraft.net')
-            .then((response) => {
-                client.user.setPresence({
-                    activities: [{
-                        name: `${response.players.online}/${response.players.max} ${response.version.name}`,
-                        type: ActivityType.Watching
-                    }],
-                    status: 'online',
-                });
+        statusMSG.edit({ embeds: [statusEmbed] })
 
-                var players;
-                if (response.players.online >= 1) {
-                    players = []
-                    for (var i = 0; i < response.players.sample.length; i++) {
-                        players.push(response.players.sample[i].name)
+        setInterval(async () => {
+            util.status('eternia.serveminecraft.net')
+                .then((response) => {
+                    client.user.setPresence({
+                        activities: [{
+                            name: `${response.players.online}/${response.players.max} ${response.version.name}`,
+                            type: ActivityType.Watching
+                        }],
+                        status: 'online',
+                    });
+
+                    var players;
+                    if (response.players.online >= 1) {
+                        players = []
+                        for (var i = 0; i < response.players.sample.length; i++) {
+                            players.push(response.players.sample[i].name)
+                        }
+                    } else if (response.players.online === 0) {
+                        players = "Nobody's online."
                     }
-                } else if (response.players.online === 0) {
-                    players = "Nobody's online."
-                }
 
-                statusEmbed.setFields(
-                    { name: 'MOTD', value: `\`${response.motd.clean}\``, inline: false },
-                    { name: 'IP:', value: `\`eternia.serveminecraft.net\``, inline: true },
-                    { name: 'Version:', value: `\`${response.version.name}\``, inline: false },
-                    { name: 'Online Players', value: `\`${response.players.online}\``, inline: true },
-                    { name: 'Max Players', value: `\`${response.players.max}\``, inline: true },
-                    { name: 'Available Slots', value: `\`${response.players.max - response.players.online}\``, inline: true },
-                    { name: 'Players', value: `\`${players}\``, inline: false },
-                )
+                    statusEmbed.setFields(
+                        { name: 'MOTD', value: `\`${response.motd.clean}\``, inline: false },
+                        { name: 'IP:', value: `\`eternia.serveminecraft.net\``, inline: true },
+                        { name: 'Version:', value: `\`${response.version.name}\``, inline: false },
+                        { name: 'Online Players', value: `\`${response.players.online}\``, inline: true },
+                        { name: 'Max Players', value: `\`${response.players.max}\``, inline: true },
+                        { name: 'Available Slots', value: `\`${response.players.max - response.players.online}\``, inline: true },
+                        { name: 'Players', value: `\`${players}\``, inline: false },
+                    )
 
-                statusEmbed.setFooter({ text: `RTL: ${response.roundTripLatency}` })
+                    statusEmbed.setFooter({ text: `RTL: ${response.roundTripLatency}` })
 
-                statusMSG.edit({ embeds: [statusEmbed] })
+                    statusMSG.edit({ embeds: [statusEmbed] })
 
 
-            }).catch((error) => {
-                client.user.setPresence({
-                    activities: [{
-                        name: `offline`,
-                        type: ActivityType.Playing
-                    }],
-                    status: 'idle',
-                });
-                statusEmbed.setFields({ name: 'State', value: '\`Offline\`' })
-                statusEmbed.setFooter({ text: "RTL: 0" })
-                statusMSG.edit({ embeds: [statusEmbed] })
-            })
-    }, 10000)
+                }).catch((error) => {
+                    client.user.setPresence({
+                        activities: [{
+                            name: `offline`,
+                            type: ActivityType.Playing
+                        }],
+                        status: 'idle',
+                    });
+                    statusEmbed.setFields({ name: 'State', value: '\`Offline\`' })
+                    statusEmbed.setFooter({ text: "RTL: 0" })
+                    statusMSG.edit({ embeds: [statusEmbed] })
+                })
+        }, 10000)
+
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 module.exports = { execute };
